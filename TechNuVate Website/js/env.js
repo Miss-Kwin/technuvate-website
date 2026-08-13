d196765f14c65c587fddec22d9771f6199cfa6d6
madjnr <danielmadubuihe@gmail.com>
2026-08-13 17:30:30 +0100

    feat(env): add Cloudinary configuration for file uploads fix(verify-labs-it): standardize string quotes and improve error handling

--- TechNuVate Website/js/env.js
+++ TechNuVate Website/js/env.js
@@ -1,3 +1,5 @@
 window.APP_ENV = window.APP_ENV || {};
 window.APP_ENV.FW_PUBLIC_KEY =
   "FLWPUBK_TEST-eba5e0f0ee9e41c6afc75a4eb1180b21-X";
+window.CLOUDINARY_CLOUD_NAME = "x88ovgjh";
+window.CLOUDINARY_UPLOAD_PRESET = "TNV_unsigned";
